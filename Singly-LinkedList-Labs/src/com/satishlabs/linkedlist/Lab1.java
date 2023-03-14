8cfbcdb7d4ce0ed087a3b75e5556d8817ffcf74d
satishlabs <spd61185@gmail.com>
2023-03-14 20:06:37 +0530

    SinglyLinkedList - traverse the linkedlist

---
+++ Singly-LinkedList-Labs/src/com/satishlabs/linkedlist/Lab1.java
@@ -0,0 +1,35 @@
+package com.satishlabs.linkedlist;
+
+//Traverse the LinkedList using Iterative style
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		Node head = new Node(10);
+		head.next = new Node(20);
+		head.next.next = new Node(30);
+		head.next.next.next = new Node(40);
+		head.next.next.next.next = new Node(50);
+
+		displayList(head);
+	}
+
+	private static void displayList(Node headNode) {
+		Node currentNode = headNode;
+		while (currentNode != null) {
+			System.out.print("[" + currentNode.data + "]" + "\t");
+			currentNode = currentNode.next;
+		}
+		System.out.println("\n");
+	}
+}
