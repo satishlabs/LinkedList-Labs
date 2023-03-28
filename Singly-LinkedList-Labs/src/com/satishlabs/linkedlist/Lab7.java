184e853550198d7bea5bdcb399a2313db3130808
satishlabs <spd61185@gmail.com>
2023-03-28 21:03:48 +0530

    Delete the Node at the begining of LinkedList

---
+++ Singly-LinkedList-Labs/src/com/satishlabs/linkedlist/Lab7.java
@@ -0,0 +1,60 @@
+package com.satishlabs.linkedlist;
+
+//Delete the Node at the begining of LinkedList
+
+public class Lab7 {
+
+	public static void main(String[] args) {
+		Node head = null;
+		head = insertLast(head, 10);
+		head = insertLast(head, 20);
+		head = insertLast(head, 30);
+		head = insertLast(head, 40);
+		head = insertLast(head, 50);
+		displayList(head);
+		System.out.println("-----------------------");
+		head = DeletFirst(head);
+		displayList(head);
+		System.out.println("-----------------------");
+		head = DeletFirst(head);
+		displayList(head);
+	}
+
+	private static void displayList(Node headNode) {
+		Node currentNode = headNode;
+		while (currentNode != null) {
+			System.out.print("[" + currentNode.data + "]" + "->\t");
+			currentNode = currentNode.next;
+		}
+		System.out.println("\n");
+	}
+
+	static Node DeletFirst(Node headNode) {
+		if (headNode == null)
+			return null;
+
+		Node temp = headNode;
+		headNode = headNode.next;
+		temp.next = null;
+		return headNode;
+	}
+
+	static Node insertLast(Node headNode, int data) {
+		Node temp = new Node(data);
+		if (headNode == null) {
+			return temp;
+		}
+
+		// To Reach the Last node of LinkedList
+		Node currentNode = headNode;
+		while (currentNode.next != null) {
+			currentNode = currentNode.next;
+		}
+
+		currentNode.next = temp;
+		return headNode;
+	}
+}
+
+//Time = O(1)
+//Space = O(1)
